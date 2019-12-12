cd76cc8421fda757f3e957bc8a48a40c213f6261
oespinosa <oliver.espinosa.meneses@gmail.com>
2019-12-12 15:30:34 -0600

    SE agrega splash

--- main.cpp
+++ main.cpp
@@ -3,14 +3,19 @@
 #include "treemodel.h"
 #include <QFile>
 #include <QTreeView>
-
+#include <QSplashScreen>
 
 int main(int argc, char *argv[])
 {
 
     QApplication a(argc, argv);
+    QPixmap pixmap("splash.png");
+    QSplashScreen *splash = new QSplashScreen(pixmap);
+    splash->show();
+    splash->showMessage("Loaded modules");
     MainWindow w;
     w.show();
+    splash->hide();
 
 
     return a.exec();
